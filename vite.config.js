e78eccb6682085e7fd7cf2ac942726a7a6ce41ef
liana89kiev-crypto <liana89kiev@gmail.com>
2026-08-04 11:06:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/dictionary/', // 👈 Обов'язково вкажіть точну назву вашого репозиторію
+  base: '/Dictionary/', // 👈 Точна назва вашого репозиторію
 });
